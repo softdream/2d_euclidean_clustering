21319a27d6ad8ee02be7963a3eb0f41682b8b960
daofeng <9779716+du-daofeng@user.noreply.gitee.com>
2022-11-21 15:59:08 +0800

    euclidean clustering

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
 	std::cout<<"--------------- EUCLIDEAN CLUSTERINT --------------"<<std::endl;
 	file::FileRead file_read;
 
-        file_read.openFile( "test_data" );
+        file_read.openFile( "test_data5" );
 
 	cluster::Cluster<float> cluster;
 
@@ -30,12 +30,21 @@ int main()
 
 		cv::waitKey(0);
 		// draw clusters
-		for( auto& cluster : clusters ){
-			for( auto& pt : cluster ){
+		//for( auto& cluster : clusters ){
+		//	for( auto& pt : cluster ){
+		for( int i = 0; i < clusters.size(); i ++ ){
+			for( auto& pt : clusters[i] ){
 				float img_x = static_cast<float>( image.cols ) / 2 + static_cast<float>( pt[0] ) * 1000;
                 		float img_y = static_cast<float>( image.rows ) / 2 - static_cast<float>( pt[1] ) * 1000;
 
-                		cv::circle(image, cv::Point2f(img_x, img_y), 2, cv::Scalar(0, 0, 255), -1);
+				if( i == 1 )
+                			cv::circle(image, cv::Point2f(img_x, img_y), 2, cv::Scalar(0, 0, 255), -1);
+				else if( i == 2 ) 
+					cv::circle(image, cv::Point2f(img_x, img_y), 2, cv::Scalar(0, 255, 0), -1);
+				else if( i == 3 )
+					cv::circle(image, cv::Point2f(img_x, img_y), 2, cv::Scalar(255, 0, 0), -1);
+				else 
+					cv::circle(image, cv::Point2f(img_x, img_y), 2, cv::Scalar(0, 255, 255), -1);
 			}
 		}
 		imshow("clusters", image);
